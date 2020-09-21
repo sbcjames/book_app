22038501b9ffc80ef01df8aff1000f7523417a7f
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2020-09-21 14:24:15 -0700

    proof of life

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const express = require('express');
 const app = express();
 require('ejs');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+app.use(express.urlencoded({extended:true}));
 app.get('/', renderHome);
 
 function renderHome(req, res){
-  res.render('index.ejs');
+  res.render('pages/index');
 }
 
 app.listen(PORT, () => {
